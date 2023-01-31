2fda3c74e981045d9d41179355cd1aef24ed0a58
username <it@sofcontraining.com>
2023-01-31 16:47:44 +0530

    news added

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,41 +3,13 @@ import React from 'react'
 export default function Newsitem(props) {
     return (
         <>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-4'>
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{props.title}</h5>
-                                <p className="card-text">{props.description}</p>
-                                <a href="/" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-4'>
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{props.title}</h5>
-                                <p className="card-text">{props.description}</p>
-                                <a href="/" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-4'>
-                        <div className="card">
-                            <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{props.title}</h5>
-                                <p className="card-text">{props.description}</p>
-                                <a href="/" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
+            <div className="card">
+                <img src={props.urlToImage} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                    <a href={props.url} className="btn btn-primary">Go somewhere</a>
                 </div>
-
-
             </div>
         </>
     )
